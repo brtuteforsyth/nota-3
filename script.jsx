7b6b859a7ef657a3d906f5813c71e334eca0f9de
brtuteforsyth <GWROSS@LIVE.COM>
2026-05-20 19:28:26 +0100

    Add files via upload

---
+++ script.jsx
@@ -0,0 +1,10 @@
+let btnel = document.queryselector("button")
+let enabelel = document.queryselector("enable")
+let disableel = document.queryselector("disable")
+
+btnel.addEventlistener("click") () => {
+alert("hello World")
+})
+
+
+enabelel.addEventlistener("change")
